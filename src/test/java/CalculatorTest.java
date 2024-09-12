d8edc7ab261d75a1ac9d446fd61ab1dd51d2a386
Alfredo Rueda <alfredoru@gmail.com>
2024-09-12 22:23:07 +0200

    force error :)

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(6, calculator.add(2, 3));
     }
 
     @Test
